16d511814d3c51ee5b72277b39939dea2546c36e
Portablefire22 <barnowlsarecool@gmail.com>
2023-05-18 04:59:07 +0100

    Fixed link, added code to remove the first time setup after running.

--- firstTimeSetup.bat
+++ firstTimeSetup.bat
@@ -1,9 +1,12 @@
-md serverFiles
-cd serverFiles
-curl https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar -o server.jar
-curl https://raw.githubusercontent.com/Portablefire22/LVU/1.19/runServer.bat -o runServer.bat
-echo '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://aka.ms/MinecraftEULA).
-#Fri May 12 18:27:47 BST 2023
-eula=true' >> eula.txt
-start runServer.bat
-
+if exist runServer.bat (
+	echo Server files already exist!
+	PAUSE
+) else (
+	md serverFiles
+	cd serverFiles
+	curl https://piston-data.mojang.com/v1/objects/8f3112a1049751cc472ec13e397eade5336ca7ae/server.jar -o server.jar
+	curl https://raw.githubusercontent.com/Portablefire22/MinecraftServerDownload/main/runServer.bat -o runServer.bat
+	echo '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://aka.ms/MinecraftEULA)	#Fri May 12 18:27:47 BST 2023
+	eula=true' >> eula.txt
+	start runServer.bat
+)
--- runServer.bat
+++ runServer.bat
@@ -1,2 +1,7 @@
+if exist firstTimeSetup.bat (
+	del firstTimeSetup.bat
+) ELSE (
+	echo Not first time, skipping deletion.
+)
 java -Xmx4096M -Xms1024M -jar server.jar nogui 
 pause
